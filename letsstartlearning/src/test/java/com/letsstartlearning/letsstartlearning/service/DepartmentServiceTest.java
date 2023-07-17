4484e598bde29e7aecb6a2143db4e4d2c1662605
Ankita Santape <ankitasantape99@gmail.com>
2023-07-17 15:21:37 +0530

    Added spring boot application

--- letsstartlearning/src/test/java/com/letsstartlearning/letsstartlearning/service/DepartmentServiceTest.java
+++ letsstartlearning/src/test/java/com/letsstartlearning/letsstartlearning/service/DepartmentServiceTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class DepartmentServiceTest {
 
     @Autowired
