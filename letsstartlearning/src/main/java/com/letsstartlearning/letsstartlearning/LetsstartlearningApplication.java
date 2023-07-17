3213d0e5d551f5d71ab58c46c40eaa0894faf589
Ankita Santape <ankitasantape99@gmail.com>
2023-07-17 14:15:58 +0530

    Added spring boot application

--- letsstartlearning/src/main/java/com/letsstartlearning/letsstartlearning/LetsstartlearningApplication.java
+++ letsstartlearning/src/main/java/com/letsstartlearning/letsstartlearning/LetsstartlearningApplication.java
@@ -3,7 +3,7 @@ package com.letsstartlearning.letsstartlearning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class LetsstartlearningApplication {
 
 	public static void main(String[] args) {
